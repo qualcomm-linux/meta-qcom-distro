6e25777adea95f918750fb13bc51bfe24af321e9
Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
2026-02-17 13:07:47 +0200

    ci/yocto-check-layer: apply workarounds
    In order to test the meta-qcom-distro we need to perform several
    changes. First of all, specify DISTRO_FEATURES matching qcom-distro,
    otherwise qcom-distro packages can't pull in dependencies like
    vulkan-tool. Second, provide workarounds for recipes outside of
    qcom-distro, which otherwise break yocto-check-layers.
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>

--- ci/yocto-check-layer.sh
+++ ci/yocto-check-layer.sh
@@ -27,6 +27,41 @@ BUILDDIR="$(mktemp -p $WORK_DIR -d -t build-yocto-check-layer-XXXX)"
 source $WORK_DIR/oe-core/oe-init-build-env $BUILDDIR
 git -c advice.detachedHead=false -c init.defaultBranch=master clone --quiet --shared $REPO_DIR meta-qcom-distro
 
+# Patch dependent layers otherwise breaking yocto-check-layer
+bitbake-layers create-layer -a meta-patch
+mkdir -p meta-patch/recipes-patch/patch
+# Taint (by forced/invalidated task) changed from nostamp(uuid4):foo to nostamp(uuid4):foo
+echo 'unset do_install[nostamp]' >> meta-patch/recipes-patch/patch/aide_%.bbappend
+# Taint (by forced/invalidated task) changed from nostamp(uuid4):foo to nostamp(uuid4):foo
+echo 'unset do_populate_registry[nostamp]' >> meta-patch/recipes-patch/patch/container-registry-populate.bbappend
+# QA error because of network access
+echo 'unset do_populate_registry[network]' >> meta-patch/recipes-patch/patch/container-registry-populate.bbappend
+# Depends on the package outside of the layer set
+echo 'EXCLUDE_FROM_WORLD = "1"' >> meta-patch/recipes-patch/patch/cockpit-machines_%.bbappend
+# Missing or unbuildable dependency chain was: ['meta-world-pkgdata', 'packagegroup-yocto-builder', 'systemd']
+echo 'inherit features_check' >> meta-patch/recipes-patch/patch/packagegroup-yocto-builder.bbappend
+echo 'REQUIRED_DISTRO_FEATURES = "systemd"' >> meta-patch/recipes-patch/patch/packagegroup-yocto-builder.bbappend
+
+# DISTRO features of qcom-distro
+echo 'DISTRO_FEATURES:append:nodistro = " \
+    bluetooth \
+    efi \
+    glvnd \
+    opencl \
+    opengl \
+    overlayfs \
+    pam \
+    pni-names \
+    ptest \
+    security \
+    tpm2 \
+    virtualization \
+    vulkan \
+    wayland \
+    wifi \
+    x11 \
+"' >> conf/local.conf
+
 # Yocto Project layer checking tool
 CMD="yocto-check-layer"
 # Layer to check
